d1fb5ad598fac8b1b02fe3c949ee68eec0a16976
pola-png <polamedian@gmail.com>
2025-09-16 20:04:10 +0100

    Update Team.js

--- models/Team.js
+++ models/Team.js
@@ -1,10 +1,12 @@
-
+// models/Team.js
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const TeamSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    logoUrl: String,
-});
+  name: { type: String, required: true },
+  logo: { type: String },        // legacy field
+  logoUrl: { type: String },     // preferred
+  country: { type: String },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Team', TeamSchema);
